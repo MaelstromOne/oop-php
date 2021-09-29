8e25a9755298c7e6c314809c6766dd6f9a5866ed
Albert <albertxgm@gmail.com>
2021-09-29 17:18:26 +0300

    Задание 3

--- Worker.php
+++ Worker.php
@@ -2,7 +2,19 @@
 
 class Worker
 {
+    const AGE_RANGES = [18, 70];
+
     public $name;
     public $age;
     public $salary;
+
+    public function checkAge(): string
+    {
+        if (self::AGE_RANGES[0] < $this->age && self::AGE_RANGES[1] > $this->age) {
+            return "возраст входит в рамки";
+        }
+        else {
+            return "возраст выходит в рамки";
+        }
+    }
 }
--- index.php
+++ index.php
@@ -9,9 +9,9 @@ $worker->salary = 1000;
 
 $worker2 = new Worker();
 $worker2->name = "Вася";
-$worker2->age = 26;
+$worker2->age = 16;
 $worker2->salary = 2000;
 
 echo "<pre>";
-echo "\nСумма зарплат Ивана и Васи " . ($worker->salary + $worker2->salary);
-echo "\nСумма возрастов Ивана и Васи " . ($worker->age + $worker2->age);
+echo "\n" . $worker->checkAge();
+echo "\n" . $worker2->checkAge();
